9c36f45a8510b73a7e8ca445939077e9bc01c886
Kyle Pollich <kpollich1@gmail.com>
2017-09-07 23:16:53 -0400

    Add basic sign in/out routes
    Includes tests!

--- test/app.test.js
+++ test/app.test.js
@@ -2,12 +2,57 @@ const app = require('../lib/app')
 const request = require('supertest')
 
 describe('server', () => {
-  test('responds with hello world', () => {
-    return request(app)
-      .get('/')
-      .then(response => {
-        expect(response.statusCode).toBe(200)
-        expect(response.text).toBe('Hello World!')
-      })
+  describe('/', () => {
+    test('no route', () => {
+      return request(app)
+        .get('/')
+        .then(response => {
+          expect(response.statusCode).toBe(404)
+        })
+    })
+  })
+
+  describe('/sign-in', () => {
+    test('valid request', () => {
+      return request(app)
+        .post('/sign-in')
+        .send({ name: 'Kyle' })
+        .then(response => {
+          expect(response.statusCode).toBe(200)
+        })
+    })
+
+    test('invalid request - missing name', () => {
+      return request(app)
+        .post('/sign-in')
+        .then(response => {
+          expect(response.body).toEqual({
+            error: { code: 422, message: 'Error - missing name' }
+          })
+          expect(response.statusCode).toBe(422)
+        })
+    })
+  })
+
+  describe('/sign-out', () => {
+    test('valid request', () => {
+      return request(app)
+        .post('/sign-out')
+        .send({ name: 'Kyle' })
+        .then(response => {
+          expect(response.statusCode).toBe(200)
+        })
+    })
+
+    test('invalid request - missing name', () => {
+      return request(app)
+        .post('/sign-out')
+        .then(response => {
+          expect(response.body).toEqual({
+            error: { code: 422, message: 'Error - missing name' }
+          })
+          expect(response.statusCode).toBe(422)
+        })
+    })
   })
 })
