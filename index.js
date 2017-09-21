b32939e9f4c5fc15a2c8ba02b7b1a3a317b8fba1
Kyle Pollich <kpollich1@gmail.com>
2017-09-21 16:31:07 -0400

    Fix bunk spacing:

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 const app = require('./lib/app')
 
-const listener = app.listen(process.env.PORT || 3000 , () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('App listening on port', listener.address().port)
 })
